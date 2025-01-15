a906f9be8326035760401188856642f2b103f5f5
Clauda Yokomoi <claudayokomoi@gmail.com>
2025-01-15 15:38:56 +0300

    Created Button component with size and shape props

--- alx-project-0x00/interfaces/index.ts
+++ alx-project-0x00/interfaces/index.ts
@@ -1,4 +1,10 @@
-export interface PageProps {
+export interface PillProps {
     title: string;
-    description?: string;
+  }
+
+  // New ButtonProps for Button component
+export interface ButtonProps {
+    title: string;
+    size: "small" | "medium" | "large";  // for the button size
+    shape: "rounded-sm" | "rounded-md" | "rounded-full"; // for the button shape
   }
