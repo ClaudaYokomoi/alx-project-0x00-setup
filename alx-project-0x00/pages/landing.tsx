aa1ed1ef716cc34c7341d27b085c8b95c9ef67e7
Clauda Yokomoi <claudayokomoi@gmail.com>
2025-01-15 15:34:08 +0300

    Update

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,7 +1,12 @@
+import Card from "@/component/Card";
+
 const Landing: React.FC = () => {
   return (
     <div>
       <h1 className="text-xl font-extralight">Landing Page</h1>
+      <Card />
+      <Card/>
+      <Card/>
     </div>
   );
 };
