80e050e648c3eee7e6203404f731a7937bbe6bdd
Clauda Yokomoi <claudayokomoi@gmail.com>
2025-02-17 19:16:15 +0300

    Update Landing page and Button component with styles prop

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -6,7 +6,10 @@ const Landing: React.FC = () => {
     <div>
       <h1 className="text-xl font-extralight">Landing Page</h1>
       <Card />
-      <Button size="medium" shape="rounded-md" styles="bg-blue-500 text-white p-2" />
+      <Button size="medium" shape="rounded-sm" styles="bg-blue-500 text-white p-2" />
+<Button size="medium" shape="rounded-md" styles="bg-blue-500 text-white p-2" />
+<Button size="medium" shape="rounded-full" styles="bg-blue-500 text-white p-2" />
+
     </div>
   );
 };
